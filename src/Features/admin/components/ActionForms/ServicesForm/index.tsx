6c30cd1ecc4e64a8dba34a83105122c0deda4623
okhmadjonov <oakhmadjonov1@gmail.com>
2024-06-10 10:15:31 +0500

    category select added

--- src/Features/admin/components/ActionForms/ServicesForm/index.tsx
+++ src/Features/admin/components/ActionForms/ServicesForm/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import LanguageFormList from "@/ui/LanguageFormList/LanguageFormList";
-import { RichTextarea, Upload } from "@/ui";
-import { useTranslations } from "next-intl";
+import { RichTextarea, Select, SelectOption, Upload } from "@/ui";
+import { useLocale, useTranslations } from "next-intl";
+import useQueryApiClient from "@/utils/useQueryApiClient";
 
 interface Props {
   data?: any;
@@ -9,8 +10,34 @@ interface Props {
 
 const ServicesForm = ({ data }: Props) => {
   const t = useTranslations("ADMIN");
+  const locale = useLocale();
+  const { data: categoryRes, isLoading } = useQueryApiClient({
+    request: {
+      url: "/Category",
+      method: "GET",
+    },
+  });
+
   return (
     <div>
+      <label className="language_label">Select Category</label>
+      <Select
+        rules={[
+          {
+            required: true,
+            message: `Please Select category!`,
+          },
+        ]}
+        name={"categoryId"}
+        loading={isLoading}
+      >
+        {categoryRes?.data?.map((item: any) => (
+          <SelectOption key={item.id}>
+            {item.title}
+            {/* {item.title[locale as keyof LocaleStringsInterface]} */}
+          </SelectOption>
+        ))}
+      </Select>
       <LanguageFormList listName="title" />
       <LanguageFormList inputType="textarea" listName="description" />
     </div>
