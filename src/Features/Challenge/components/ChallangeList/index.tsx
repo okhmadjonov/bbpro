0e56561c66d5300e9e0f8b0862c203b0985abf72
okhmadjonov <oakhmadjonov1@gmail.com>
2024-06-13 02:48:56 -0700

    FAQ added

--- src/Features/Challenge/components/ChallangeList/index.tsx
+++ src/Features/Challenge/components/ChallangeList/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from  './ChallengeList.module.scss';
-import PhotoGallery from '../ChallangeCard';
-
-
+import React from "react";
+import styles from "./ChallengeList.module.scss";
+import PhotoGallery from "../ChallangeCard";
+import Faq from "../Faq/Faq";
 
 const ChallengeList = () => {
   return (
-      <div className={styles.challengelist}>
-          <PhotoGallery/>
+    <div className={styles.challengelist}>
+      <Faq />
+      <PhotoGallery />
     </div>
-  )
-}
+  );
+};
 
 export default ChallengeList;
