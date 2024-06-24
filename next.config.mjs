8577ba333bb92f6bc5e4739d2ea888ebe7c7d8df
okhmadjonov <oakhmadjonov1@gmail.com>
2024-06-24 06:01:16 -0700

    new changes for tab

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     defaultLocale: "ru",
   },
   images: {
-    domains: ["bbpro.me", "localhost"],
+    domains: ["localhost", "bbpro.me"],
   },
   transpilePackages: [
     "@ant-design",
