ad76ce4222d1facf60cd10c456d632051c254068
okhmadjonov <oakhmadjonov1@gmail.com>
2024-07-23 23:11:54 -0700

    vertical line almost done

--- src/Features/Service/components/ServiceDetails/components/Timeline.tsx
+++ src/Features/Service/components/ServiceDetails/components/Timeline.tsx
@@ -1,50 +1,43 @@
 import React, { useState } from "react";
 import "react-vertical-timeline-component/style.min.css";
 import styles from "./Timeline.module.scss";
+import Image from "next/image";
+
 const timelineData = [
   {
-    text: "Этапы наших работ",
-    date: "February 25 2019",
-
+    text: "1) Сбор информации",
     link: {
       url: "https://github.com/florinpop17/app-ideas",
-      text: "Check it out on GitHub",
+      text: "Check it out here",
     },
   },
   {
-    text: "Этапы наших работ",
-    date: "March 04 2019",
-
+    text: "2) Создание структуры и карты сайта",
     link: {
       url: "https://florin-pop/blog/2019/03/weekly-coding-challenge/",
       text: "Check it out here",
     },
   },
   {
-    text: "Этапы наших работ",
-    date: "March 07 2019",
+    text: "3) Разработка дизайна",
 
     link: {
       url: "https://twitter.com/florinpop1705",
-      text: "See profile",
+      text: "Check it out here",
     },
   },
   {
-    text: "Этапы наших работ",
-    date: "March 18 2019",
-
+    text: "4) Узнаваемость бренда",
     link: {
       url: "https://medium.freecodecamp.org/how-to-build-a-double-slider-sign-in-and-sign-up-form-6a5d03612a34",
       text: "Check it out here",
     },
   },
   {
-    text: "Этапы наших работ",
-    date: "April 05 2019",
-
+    text: "5) Тестирование и запуск",
     link: {
       url: "https://medium.com/@popflorin1705",
-      text: "See profile",
+      text: "Check it out here",
     },
   },
 ];
