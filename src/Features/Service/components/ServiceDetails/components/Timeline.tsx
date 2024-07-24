f65922a66097cd8aa9859f64ef12aa017c0a312b
okhmadjonov <oakhmadjonov1@gmail.com>
2024-07-23 22:51:35 -0700

    design changed on timeline card

--- src/Features/Service/components/ServiceDetails/components/Timeline.tsx
+++ src/Features/Service/components/ServiceDetails/components/Timeline.tsx
@@ -5,10 +5,7 @@ const timelineData = [
   {
     text: "Этапы наших работ",
     date: "February 25 2019",
-    category: {
-      tag: "app-ideas",
-      color: "#FFDB14",
-    },
+
     link: {
       url: "https://github.com/florinpop17/app-ideas",
       text: "Check it out on GitHub",
@@ -17,10 +14,7 @@ const timelineData = [
   {
     text: "Этапы наших работ",
     date: "March 04 2019",
-    category: {
-      tag: "blog",
-      color: "#e17b77",
-    },
+
     link: {
       url: "https://florin-pop/blog/2019/03/weekly-coding-challenge/",
       text: "Check it out here",
@@ -29,10 +23,7 @@ const timelineData = [
   {
     text: "Этапы наших работ",
     date: "March 07 2019",
-    category: {
-      tag: "twitter",
-      color: "#1DA1F2",
-    },
+
     link: {
       url: "https://twitter.com/florinpop1705",
       text: "See profile",
@@ -41,10 +32,7 @@ const timelineData = [
   {
     text: "Этапы наших работ",
     date: "March 18 2019",
-    category: {
-      tag: "medium",
-      color: "#018f69",
-    },
+
     link: {
       url: "https://medium.freecodecamp.org/how-to-build-a-double-slider-sign-in-and-sign-up-form-6a5d03612a34",
       text: "Check it out here",
@@ -53,10 +41,7 @@ const timelineData = [
   {
     text: "Этапы наших работ",
     date: "April 05 2019",
-    category: {
-      tag: "medium",
-      color: "#018f69",
-    },
+
     link: {
       url: "https://medium.com/@popflorin1705",
       text: "See profile",
@@ -67,10 +52,6 @@ const timelineData = [
 const TimelineItem = ({ data }: any) => (
   <div className={styles.timelineitem}>
     <div className={styles.timelineitemcontent}>
-      <span className={styles.tag} style={{ background: data.category.color }}>
-        {data.category.tag}
-      </span>
-      <time>{data.date}</time>
       <p>{data.text}</p>
       {data.link && (
         <a href={data.link.url} target="_blank" rel="noopener noreferrer">
